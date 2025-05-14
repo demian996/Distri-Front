4e84d11bf155ef788a51896693525e396bfe6662
Johao Crespo <johao1999crespo@hotmail.com>
2025-05-14 15:46:23 -0500

    add:files for the front

---
+++ script.js
@@ -0,0 +1,14 @@
+function DigitalOcean() {
+  window.location.href = "https://plankton-app-g6v6z.ondigitalocean.app/";
+}
+
+function Vercel() {
+  window.location.href = "https://hola-mundo-theta.vercel.app/";
+}
+function Railway() {
+  window.location.href = "";
+}
+function Heroku() {
+  window.location.href = "";
+}
+
